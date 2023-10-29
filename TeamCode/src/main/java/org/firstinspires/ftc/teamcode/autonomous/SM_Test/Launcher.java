f40476b44d1a7117fe02150e8618d38ff4857062
Mithilessh2010 <Mithib2010@hotmail.com>
2023-10-29 11:29:56 -0700

    Renamed Arm_test file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SM_Test/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SM_Test/Launcher.java
@@ -9,24 +9,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Launcher extends LinearOpMode{
     DcMotorEx launcher;
-    Servo alex;
+    //Servo alex;
     boolean press = false;
 
     public void runOpMode(){
         launcher = hardwareMap.get(DcMotorEx.class, "launcher");
-        alex = hardwareMap.get(Servo.class, "alex");
+        //alex = hardwareMap.get(Servo.class, "alex");
 
         while(opModeIsActive()) {
             if (gamepad1.a && press == false) {
                 launcher.setVelocity(100);
                 sleep(2000);
-                alex.setPosition(0.25);
+              //  alex.setPosition(0.25);
                 boolean press = true;
                 telemetry.addData("Launching", "Shreesh");
             }
             if (gamepad1.a && press == true) {
                 launcher.setVelocity(0);
-                alex.setPosition(0);
+               // alex.setPosition(0);
                 boolean press = false;
                 telemetry.addData("Stopped", "Shreesh");
             }
