59bc41355e61880bf6e7069ce8cc7437295090ff
Mithilessh2010 <Mithib2010@hotmail.com>
2023-11-01 13:40:48 -0700

    Launcher done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Launcher.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 @TeleOp(name="launcher", group="SM OpMode")
 public class Launcher extends LinearOpMode{
     DcMotorEx launcher;
@@ -16,11 +17,26 @@ public class Launcher extends LinearOpMode{
         launcher = hardwareMap.get(DcMotorEx.class, "launcher");
         //push = hardwareMap.get(Servo.class, "push");
 
+        boolean previousa = false;
+
         waitForStart();
         while(opModeIsActive()) {
+            if(gamepad1.a && press == false) {
                 launcher.setPower(0.1);
                 telemetry.addData("Launching", "Shreesh");
                 telemetry.update();
+                press = true;
+                previousa = true;
+            }
+            else if(gamepad1.a && press == true && previousa == true){
+                launcher.setPower(0);
+                telemetry.addData("Stopping Launch", "Shreesh");
+                telemetry.update();
+                press = false;
+            }
+
+            previousa = gamepad1.a;
+
         }
     }
 }
