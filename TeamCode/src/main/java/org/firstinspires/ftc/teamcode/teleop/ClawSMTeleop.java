dba9cf670ff7ff4aad22ee41059f99d246b617d3
Mithilessh2010 <Mithib2010@hotmail.com>
2023-11-10 12:03:34 -0800

    ClawSMTeleop created and coded

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ClawSMTeleop.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.autonomous.SM_Test.driveSM;
+
+public class ClawSMTeleop extends LinearOpMode{
+    public enum STATE {
+        DRIVER_PRESSED_2TL,
+        DRIVER_PRESSED_2TR
+    }
+
+    public enum EVENT {
+        CLAW_OPEN,
+        CLAW_CLOSE
+    }
+
+    STATE currentState;
+    EVENT currentEvent;
+
+    public void transition(EVENT event) {
+        switch (event) {
+            case CLAW_OPEN:
+                currentState = STATE.DRIVER_PRESSED_2TL;
+                break;
+
+            case CLAW_CLOSE:
+                currentState = STATE.DRIVER_PRESSED_2TR;
+                break;
+        }
+    }
+
+    public void update() {
+        switch (currentState) {
+            case DRIVER_PRESSED_2TL:
+                break;
+            case DRIVER_PRESSED_2TR:
+                break;
+        }
+    }
+    public void runOpMode(){
+            waitForStart();
+
+            while(opModeIsActive()) {
+                if(gamepad2.left_trigger > 0){
+                    transition(EVENT.CLAW_OPEN);
+                    telemetry.addData("Left Trigger Pressed, Claw Open","Claw Open");
+                    telemetry.update();
+                }
+                else if(gamepad2.right_trigger > 0){
+                    transition(EVENT.CLAW_CLOSE);
+                    telemetry.addData("Right Trigger Pressed, Claw Closed","Claw Closed");
+                    telemetry.update();
+                }
+            }
+    }
+}
